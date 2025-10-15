c9357b58baba2e03ebaf6ed9d5c7b54c9c142a32
Adriel <adxqh14@gmail.com>
2025-10-14 20:49:20 -0400

    Ahora si

--- db.js
+++ db.js
@@ -1,15 +1,23 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+console.log('Conectando a DB con:', process.env.DB_NAME, process.env.DB_USER, process.env.DB_HOST);
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME,   // nombre de tu DB, ej: onepiece
-  process.env.DB_USER,   // tu usuario de PostgreSQL
-  process.env.DB_PASS,   // tu contraseña
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
-    logging: false
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
