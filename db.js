cafe471c4a5a0b25b283deed4b6788f3bef33f83
Adriel <adxqh14@gmail.com>
2025-10-16 07:54:41 -0400

    Ahora si OHOH

--- db.js
+++ db.js
@@ -1,24 +1,15 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-console.log('Conectando a DB con:', process.env.DB_NAME, process.env.DB_USER, process.env.DB_HOST);
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
-);
+  },
+  logging: false,
+});
 
 module.exports = sequelize;
